08475b05f9ac457ee1b4378cc6b49fc6b75eb722
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-09-11 05:08:21 +0300

    navigation to home screen

--- lib/views/welcome_view.dart
+++ lib/views/welcome_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:tasky_app/views/home_view.dart';
 
 class WelcomeView extends StatelessWidget {
   const WelcomeView({super.key});
@@ -9,68 +10,67 @@ class WelcomeView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff181818),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/images/logo.svg',
-                  width: 42,
-                  height: 42,
-                ),
-                SizedBox(width: 16),
-                Text(
-                  'Tasky',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 28,
-                    color: Color(0xffFFFFFF),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    'assets/images/logo.svg',
+                    width: 42,
+                    height: 42,
                   ),
-                ),
-              ],
-            ),
-
-            SizedBox(height: 118),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome To Tasky ',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    fontSize: 24,
-                    color: Color(0xffFFFCFC),
+                  SizedBox(width: 16),
+                  Text(
+                    'Tasky',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 28,
+                      color: Color(0xffFFFFFF),
+                    ),
                   ),
-                ),
-                SvgPicture.asset(
-                  'assets/images/wave_hand.svg',
-                  width: 28,
-                  height: 28,
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Your productivity journey starts here.',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 16,
-                color: Color(0xffFFFCFC),
+                ],
               ),
-            ),
-            SizedBox(height: 24),
-            SvgPicture.asset(
-              'assets/images/welcome.svg',
-              width: 215,
-              height: 200,
-            ),
-            SizedBox(height: 28),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Column(
+              SizedBox(height: 118),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Welcome To Tasky ',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 24,
+                      color: Color(0xffFFFCFC),
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    'assets/images/wave_hand.svg',
+                    width: 28,
+                    height: 28,
+                  ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Your productivity journey starts here.',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 16,
+                  color: Color(0xffFFFCFC),
+                ),
+              ),
+              SizedBox(height: 24),
+              SvgPicture.asset(
+                'assets/images/welcome.svg',
+                width: 215,
+                height: 200,
+              ),
+              SizedBox(height: 28),
+              Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
@@ -109,25 +109,34 @@ class WelcomeView extends StatelessWidget {
                   ),
                 ],
               ),
-            ),
-            SizedBox(height: 24),
-
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xff15B86C),
-                foregroundColor: Color(0xffFFFCFC),
-                fixedSize: Size(343, 40),
-              ),
-              onPressed: () {},
-              child: Text(
-                'Let’s Get Started',
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
+              SizedBox(height: 24),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xff15B86C),
+                  foregroundColor: Color(0xffFFFCFC),
+                  fixedSize: Size(
+                    MediaQuery.of(context).size.width,
+                    40,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => HomeView(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Let’s Get Started',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
