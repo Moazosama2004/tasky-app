656c9337721fec394f68b6f23d9c38b6d2dc806d
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-09-14 23:14:23 +0300

    changes on profile view with stack widget for prfole picture

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,14 +1,37 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class ProfileView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  String username = '';
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  _loadUserData() async {
+    final prefs = await SharedPreferences.getInstance();
+    username = prefs.getString('username') ?? '';
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding: const EdgeInsets.all(18.0),
+          padding: const EdgeInsets.all(16.0),
           child: Text(
             'My Profile',
             style: TextStyle(
@@ -16,6 +39,76 @@ class ProfileView extends StatelessWidget {
               fontWeight: FontWeight.w400,
               fontSize: 20,
             ),
+          ),
+        ),
+        SizedBox(height: 16),
+        Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 42.5,
+                    backgroundImage: AssetImage(
+                      'assets/images/profile_avatar.png',
+                    ),
+                  ),
+
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: GestureDetector(
+                      onTap: () {
+                        log('CLICKED');
+                      },
+                      child: CircleAvatar(
+                        radius: 17,
+                        backgroundColor: Color(0xff282828),
+                        child: SvgPicture.asset(
+                          'assets/images/camera.svg',
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  username,
+                  style: TextStyle(
+                    color: Color(0xffFFFCFC),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Text(
+                'One task at a time. One step closer.',
+                style: TextStyle(
+                  color: Color(0xffC6C6C6),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 24),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            children: [
+              Text(
+                'Profile Info',
+                style: TextStyle(
+                  color: Color(0xffFFFCFC),
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+            ],
           ),
         ),
       ],
